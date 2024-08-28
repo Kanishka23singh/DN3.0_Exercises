504e56d56f910a457f9c2236a8c5a3fac88696f7
Kanishka Singh <92868964+Kanishka23singh@users.noreply.github.com>
2024-08-28 22:49:33 +0530

    Delete Week 5_Spring REST directory

--- Week 5_Spring REST/Task 10 - 12/src/main/java/com/library/BookStoreAPI/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.library.BookStoreAPI.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
